f6438f66f1445b014b52b5070e98c82251a18ea0
FVieira <f.de.moraes.tavares@mobgen.com>
2019-10-15 10:33:02 +0200

    extracted jupyter notebook into files

---
+++ run.py
@@ -0,0 +1,174 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import time
+from transformer import Transformer, CustomSchedule
+from padding import create_look_ahead_mask, create_padding_mask
+
+MAX_LENGTH = 40
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+# Input definition
+examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
+                               as_supervised=True)
+train_examples, val_examples = examples['train'], examples['validation']
+tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    (en.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    (pt.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+def encode(lang1, lang2):
+  lang1 = [tokenizer_pt.vocab_size] + tokenizer_pt.encode(
+      lang1.numpy()) + [tokenizer_pt.vocab_size+1]
+
+  lang2 = [tokenizer_en.vocab_size] + tokenizer_en.encode(
+      lang2.numpy()) + [tokenizer_en.vocab_size+1]
+  
+  return lang1, lang2
+
+def tf_encode(pt, en):
+  return tf.py_function(encode, [pt, en], [tf.int64, tf.int64])
+
+def filter_max_length(x, y, max_length=MAX_LENGTH):
+  return tf.logical_and(tf.size(x) <= max_length,
+                        tf.size(y) <= max_length)
+
+train_dataset = train_examples.map(tf_encode)
+train_dataset = train_dataset.filter(filter_max_length)
+# cache the dataset to memory to get a speedup while reading from it.
+train_dataset = train_dataset.cache()
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(
+    BATCH_SIZE, padded_shapes=([-1], [-1]))
+train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+val_dataset = val_examples.map(tf_encode)
+val_dataset = val_dataset.filter(filter_max_length).padded_batch(
+    BATCH_SIZE, padded_shapes=([-1], [-1]))
+
+# ----------------- HYPERPARAMETERS -----------------
+num_layers = 4
+d_model = 128
+dff = 512
+num_heads = 8
+
+input_vocab_size = tokenizer_pt.vocab_size + 2
+target_vocab_size = tokenizer_en.vocab_size + 2
+dropout_rate = 0.1
+# ----------------- HYPERPARAMETERS -----------------
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+  mask = tf.math.logical_not(tf.math.equal(real, 0))
+  loss_ = loss_object(real, pred)
+
+  mask = tf.cast(mask, dtype=loss_.dtype)
+  loss_ *= mask
+  
+  return tf.reduce_mean(loss_)
+
+learning_rate = CustomSchedule(d_model)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, 
+                                     epsilon=1e-9)
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+    name='train_accuracy')
+
+transformer = Transformer(num_layers, d_model, num_heads, dff,
+                          input_vocab_size, target_vocab_size, 
+                          pe_input=input_vocab_size, 
+                          pe_target=target_vocab_size,
+                          rate=dropout_rate)
+
+def create_masks(inp, tar):
+  # Encoder padding mask
+  enc_padding_mask = create_padding_mask(inp)
+  
+  # Used in the 2nd attention block in the decoder.
+  # This padding mask is used to mask the encoder outputs.
+  dec_padding_mask = create_padding_mask(inp)
+  
+  # Used in the 1st attention block in the decoder.
+  # It is used to pad and mask future tokens in the input received by 
+  # the decoder.
+  look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+  dec_target_padding_mask = create_padding_mask(tar)
+  combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+  
+  return enc_padding_mask, combined_mask, dec_padding_mask
+
+checkpoint_path = "./checkpoints/train"
+
+ckpt = tf.train.Checkpoint(transformer=transformer,
+                           optimizer=optimizer)
+
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+# if a checkpoint exists, restore the latest checkpoint.
+if ckpt_manager.latest_checkpoint:
+  ckpt.restore(ckpt_manager.latest_checkpoint)
+  print ('Latest checkpoint restored!!')
+
+# The @tf.function trace-compiles train_step into a TF graph for faster
+# execution. The function specializes to the precise shape of the argument
+# tensors. To avoid re-tracing due to the variable sequence lengths or variable
+# batch sizes (the last batch is smaller), use input_signature to specify
+# more generic shapes.
+
+train_step_signature = [
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+]
+
+@tf.function(input_signature=train_step_signature)
+def train_step(inp, tar):
+  tar_inp = tar[:, :-1]
+  tar_real = tar[:, 1:]
+  
+  enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inp, tar_inp)
+  
+  with tf.GradientTape() as tape:
+    predictions, _ = transformer(inp, tar_inp, 
+                                 True, 
+                                 enc_padding_mask, 
+                                 combined_mask, 
+                                 dec_padding_mask)
+    loss = loss_function(tar_real, predictions)
+
+  gradients = tape.gradient(loss, transformer.trainable_variables)    
+  optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+  
+  train_loss(loss)
+  train_accuracy(tar_real, predictions)
+
+EPOCHS = 20
+
+for epoch in range(EPOCHS):
+  start = time.time()
+  
+  train_loss.reset_states()
+  train_accuracy.reset_states()
+  
+  # inp -> portuguese, tar -> english
+  for (batch, (inp, tar)) in enumerate(train_dataset):
+    train_step(inp, tar)
+    
+    if batch % 50 == 0:
+      print ('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+          epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+      
+  if (epoch + 1) % 5 == 0:
+    ckpt_save_path = ckpt_manager.save()
+    print ('Saving checkpoint for epoch {} at {}'.format(epoch+1,
+                                                         ckpt_save_path))
+    
+  print ('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1, 
+                                                train_loss.result(), 
+                                                train_accuracy.result()))
+
+  print ('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
